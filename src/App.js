4c065ba990f98e468c5a6b75f27ae7828bfe0214
Daniel-Uchay <meetcaxtroh@gmail.com>
2023-09-01 16:36:39 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const [incr, setincr] = useState(0)
+    return (
+        <div className="App">
+            <h3>{incr}</h3>
+            <button type="submit" onClick={()=> setincr(incr + 1)}>Click Me</button>
+        </div>
+    );
 }
 
 export default App;
